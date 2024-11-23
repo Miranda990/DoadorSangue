97a47ea6ec06b03ec9cbf6ad37b918809c7f8bed
Miranda990 <168492604+Miranda990@users.noreply.github.com>
2024-11-22 21:19:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Digite aqui sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade <= 67)
+{
+    Console.WriteLine("Pode doar Sangue!");
+
+}
+else{
+    Console.WriteLine("Não pode Doar Sangue!");
+}
